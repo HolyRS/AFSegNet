5f2d46a03df28400573c6965b63bb0be1489f22f
HolyRS <24210812000019@hainanu.edu.cn>
2026-06-15 22:48:34 +0800

    Delete model directory

--- model/analyze/mmpretrain_configs/configs/efficientnet_v2/efficientnetv2-l_8xb32_in21k.py
+++
@@ -1,4 +0,0 @@
-_base_ = ['./efficientnetv2-s_8xb32_in21k.py']
-
-# unetmamba setting
-model = dict(backbone=dict(arch='l'), )
--- model/analyze/mmpretrain_configs/configs/twins/twins-svt-small_8xb128_in1k.py
+++
@@ -1,4 +0,0 @@
-_base_ = ['twins-svt-base_8xb128_in1k.py']
-
-# unetmamba settings
-model = dict(backbone=dict(arch='small'), head=dict(in_channels=512))
